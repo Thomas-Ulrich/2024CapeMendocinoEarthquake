51f806fc3516e361f8a124b06c6ec2c0e6ce95ac
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2025-02-27 18:11:38 +0100

    add dynamic rupture scripts2

---
+++ figures/figure_dynamic_rupture/generate_colorbars.sh
@@ -0,0 +1,4 @@
+generate_color_bar davos_r --crange 0 3 --labelfont 12 --height 1.4 3 --nticks 3
+generate_color_bar lipari_r --crange 0 4 --labelfont 12 --height 1.4 3 --nticks 3
+generate_color_bar lapaz_r --crange 0 6 --labelfont 12 --height 1.3 4 --nticks 3
+generate_color_bar magma_r --crange 0 2 --labelfont 12 --height 1.3 4 --nticks 3
