22a40a54f241f52301249b6b934fe042d48f5553
Yohai Magen <ymagen@yuval-2.local>
2025-03-21 17:01:11 -0700

    remove backround sesmisity from lomax in figure 1

--- figures/figure1/plot.py
+++ figures/figure1/plot.py
@@ -7,8 +7,11 @@ background_cat = pd.read_csv('./background_cat.csv')
 cmt_small = pd.read_csv('./cmt_cat_small.csv')
 cmt_large = pd.read_csv('./cmt_cat_large.csv')
 cat = pd.read_csv('./cat.csv')
+cat['time'] = pd.to_datetime(cat['time'])
+cat = cat[cat.time > pd.to_datetime('2024-12-05T18:44:21')]
 back_projection = pd.read_csv('./back_projection.csv')
 pygmt.makecpt(cmap='hot', series=[0, 15, 0.1], continuous=True, output='back_projection.cpt')
+kinematic_inv_stations = pd.read_csv('./kinematic_inv_stations.csv')
 
 
 fig = pygmt.Figure()
@@ -89,6 +92,8 @@ fig.text(x=-128.9, y=45.5, text="JdFP", font="6p", angle=0)
 fig.plot(x=-127.8, y=45.5, style="v0.3c+ea+h0.1+a35", direction=([30], [0.6]), pen="1p", fill="black")
 fig.text(x=-120.5, y=43.0, text="North American Plate", font="6p", angle=0)
 fig.plot(x=[region1[0], region1[1], region1[1], region1[0]], y=[region1[2], region1[2], region1[3], region1[3]], pen="2p,red", close=True)
+fig.text(position='TL', text='(a)', font='12p,Helvetica-Bold', offset='-0.5c/0.5c', no_clip=True)
+
 # ##############################################################################################################################################################################################################################################################################
 # ##############################################################################################################################################################################################################################################################################
 # ##############################################################################################################################################################################################################################################################################
@@ -162,4 +167,44 @@ with pygmt.config(FONT_LABEL='24p,Helvetica,black', FONT_ANNOT_PRIMARY='24p,Helv
         frame='af+ltime (s)' # Label the color bar
     )
 fig.basemap(map_scale="n0.1/0.1+w10k+f+u")
+
+fig.shift_origin(xshift='21c')
+t_stations = kinematic_inv_stations[kinematic_inv_stations.type.isin(['SM', 'GPS'])]
+
+region = [-125.2, t_stations.lon.max()+0.1, t_stations.lat.min()-0.1, t_stations.lat.max()+0.1]
+pygmt.config(FORMAT_GEO_MAP='ddd.xx', MAP_FRAME_TYPE='plain')
+fig.coast(region=region, projection='M10.98c', frame=['lSnE', 'xa1.0', 'ya2'], land='darkgray', water='lightblue', area_thresh=10000)
+fig.text(position='TL', text='(b)', font='12p,Helvetica-Bold', offset='-0.5c/0.5c', no_clip=True)
+
+t_stations = kinematic_inv_stations[kinematic_inv_stations.type.isin(['GPS'])]
+fig.plot(x=t_stations.lon, y=t_stations.lat, style='t0.3c', fill='green', label='GNSS')
+t_stations = kinematic_inv_stations[kinematic_inv_stations.type.isin(['SM'])]
+fig.plot(x=t_stations.lon, y=t_stations.lat, style='t0.3c', fill='blue', label='Strong Motion')
+fig.plot(x=[10], y=[10], style='t0.3c', fill='black', label='Surface Waves')
+fig.plot(x=[10], y=[10], style='s0.2c', fill='cyan', label='Teleseismic')
+
+
+
+# Event information (longitude, latitude, depth) and location for beachball
+event_data = pd.DataFrame({
+    "longitude": [-125.022],
+    "latitude": [40.374],
+    "depth": [10.0]
+})
+
+fig.plot(x=event_data.longitude, y=event_data.latitude, style='a0.6c', fill='yellow', pen='0.2p,black')
+fig.basemap(map_scale="n0.85/0.05+w50k+f+u")
+fig.legend(position='n1/1+jTR', box='+gwhite+p1p')
+
+
+fig.shift_origin(xshift='0.1c', yshift='0.1c')
+fig.coast(region='g', projection='A280/30/2i', frame=['xg20', 'yg20'], land='darkgray', water='lightblue', area_thresh=10000)
+t_stations = kinematic_inv_stations[kinematic_inv_stations.type.isin(['surf'])]
+fig.plot(x=t_stations.lon, y=t_stations.lat, style='t0.3c', fill='black')
+t_stations = kinematic_inv_stations[kinematic_inv_stations.type.isin(['body'])]
+fig.plot(x=t_stations.lon, y=t_stations.lat, style='s0.2c', fill='cyan')
+fig.plot(x=event_data.longitude, y=event_data.latitude, style='a0.5c', fill='yellow', pen='0.2p,black')
+
+# # fig.savefig('./figures/fig1.pdf')
+fig.show(width=1200)
 fig.savefig('./fig1.pdf')
