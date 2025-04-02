687dcb24fbaeb8018ab7b3cf7ee1e21d77ce2302
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2025-04-02 15:02:39 +0200

    add beam power to fig2

--- figures/figure2/plot.py
+++ figures/figure2/plot.py
@@ -49,9 +49,11 @@ stf = pd.read_csv('./stf.csv')
 stf = stf[stf.time <= 21]
 stf = pd.concat([stf, pd.DataFrame([[21, 0]], columns=['time', 'Moment_Rate'])])
 
+back_projection = pd.read_csv('../figure1/back_projection.csv')
 
-plot_kinematic = False
-plot_seismicity  = False
+
+plot_kinematic = True
+plot_seismicity  = True
 
 
 region=[-125.3, -123.6, 40, 41]
@@ -174,23 +176,27 @@ for along_dip in range(10):
         dist_stk += 3.7
     dist_dip -= 2.8
 cc = 100
-fig.plot(x = background.along_stk_disloc, y=-background.depth, style='c0.05c', fill=f'{cc}/{cc}/{cc}')
-fig.plot(x = cat_on_fault.along_stk_disloc, y=-cat_on_fault.depth, style='c0.1c', fill='cyan')
-fig.plot(x = repeating_cat.along_stk_disloc, y=-repeating_cat.depth, style='c0.1c', fill='red')
-fig.plot(x = mainshock.along_stk_disloc, y=-mainshock.depth, style='a0.5c', fill='yellow', pen='0.5p,black')
+if plot_seismicity:
+    fig.plot(x = background.along_stk_disloc, y=-background.depth, style='c0.05c', fill=f'{cc}/{cc}/{cc}')
+    fig.plot(x = cat_on_fault.along_stk_disloc, y=-cat_on_fault.depth, style='c0.1c', fill='cyan')
+    fig.plot(x = repeating_cat.along_stk_disloc, y=-repeating_cat.depth, style='c0.1c', fill='red')
+    fig.plot(x = mainshock.along_stk_disloc, y=-mainshock.depth, style='a0.5c', fill='yellow', pen='0.5p,black')
 fig.text(position='TL', no_clip=True, text='(b)', font='12p,Helvetica,black', offset='-0.8c/0.5c')
 with pygmt.config(FONT_LABEL='24p,Helvetica,black', FONT_ANNOT_PRIMARY='24p,Helvetica,black'):
     fig.colorbar(frame=['xa1f0.5+lSlip [m]', 'ya0.5'], position='g90/-3.5+w4c/0.3c', cmap='slip.cpt')
 
 
-
 ### panel b inset ###
 fig.shift_origin(yshift='0.0c', xshift='0.0c')
 with pygmt.config(FONT_LABEL='8p,Helvetica-Bold,black', FONT_ANNOT_PRIMARY='8p,Helvetica-Bold,black'):
-    fig.basemap(projection='X4c/3c', region=[0.0001, 20.9999, 0.001, 5], frame=['tblr+gwhite'])
+    fig.basemap(projection='X4c/3c', region=[0.0001, 20.9999, 0.001, 6], frame=['tblr+gwhite'])
 fig.plot(x=stf.time, y=stf.Moment_Rate / 1e18, pen='0.3p,black', close=True, fill='gray')
+ymax = np.amax(stf.Moment_Rate / 1e18)
+fig.plot(x=back_projection.time, y=ymax * back_projection.beam_power, pen='0.3p,blue', close=False)
 with pygmt.config(FONT_LABEL='6p,Helvetica-Bold,black', FONT_ANNOT_PRIMARY='6p,Helvetica-Bold,black', MAP_FRAME_TYPE='inside'):
-    fig.basemap(projection='X4c/3c', region=[0.0001, 39.999, 0.001, 5], frame=['lNbE', 'xa10f5+ltime [s]', 'ya1+lMoment Rate [Nm @[10^{18}@[]'])
+    fig.basemap(projection='X4c/3c', region=[0.0001, 39.999, 0.001, 6], frame=['lNbE', 'xa10f5+ltime [s]', 'ya1+lMoment Rate [Nm @[10^{18}@[]'])
+with pygmt.config(FONT_LABEL='6p,Helvetica-Bold,blue', FONT_ANNOT_PRIMARY='6p,Helvetica-Bold,blue', MAP_FRAME_TYPE='inside'):
+    fig.basemap(projection='X4c/3c', region=[0.0001, 39.999, 0.001, 6/ymax], frame=['lW', 'ya0.5+lBeam Power'])
 
 
 ### panel a #####
@@ -199,12 +205,12 @@ fig.basemap(projection='X18.7c/5.6c', region=[0, 88.8, -20., 0], frame=['WbNr', 
 for i, row in static_inv.iterrows():
     fig.plot(x=[row.r, row.l, row.l, row.r], y=-np.array([row.t, row.t, row.b, row.b]), pen='0.5p,gray', close=True, fill=cpt(row.slip))
 cc = 100
-fig.plot(x = background.along_stk_disloc, y=-background.depth, style='c0.05c', fill=f'{cc}/{cc}/{cc}', label='background seismicity')
-fig.plot(x = cat_on_fault.along_stk_disloc, y=-cat_on_fault.depth, style='c0.1c', fill='cyan', label='aftershocks')
-fig.plot(x = repeating_cat.along_stk_disloc, y=-repeating_cat.depth, style='c0.1c', fill='red', label='repeating earthquakes')
-fig.plot(x = mainshock.along_stk_disloc, y=-mainshock.depth, style='a0.5c', fill='yellow', pen='0.5p,black')
 
-
+if plot_seismicity:
+    fig.plot(x = background.along_stk_disloc, y=-background.depth, style='c0.05c', fill=f'{cc}/{cc}/{cc}', label='background seismicity')
+    fig.plot(x = cat_on_fault.along_stk_disloc, y=-cat_on_fault.depth, style='c0.1c', fill='cyan', label='aftershocks')
+    fig.plot(x = repeating_cat.along_stk_disloc, y=-repeating_cat.depth, style='c0.1c', fill='red', label='repeating earthquakes')
+    fig.plot(x = mainshock.along_stk_disloc, y=-mainshock.depth, style='a0.5c', fill='yellow', pen='0.5p,black')
 
 fig.text(position='TL', no_clip=True, text='(a)', font='12p,Helvetica,black', offset='-0.8c/0.5c')
 fig.legend(position='n0.01/0.01', box='+ggray')
