a7bca45281a2beac64f6bc356179b35d6aba15ff
Yohai Magen <ymagen@yuval-2.local>
2025-04-02 12:21:57 -0700

    updating fig1 and fig 2

--- figures/figure2/plot.py
+++ figures/figure2/plot.py
@@ -7,7 +7,7 @@ class Cpt:
         cpt = pd.read_csv(cpt, sep='\t', header=None)
         self.cpt_o = cpt.iloc[-3:]
         cpt = cpt[:-3]
-        cpt[0] = np.asfarray(cpt[0])
+        cpt[0] = np.array(cpt[0]).astype(float)
         self.cpt = cpt
     def __call__(self, value):
         if value is None:
@@ -51,6 +51,8 @@ stf = pd.concat([stf, pd.DataFrame([[21, 0]], columns=['time', 'Moment_Rate'])])
 
 back_projection = pd.read_csv('../figure1/back_projection.csv')
 
+span_2024={'lon':[-125.247, -124.490], 'lat':[40.383, 40.303]}
+
 
 plot_kinematic = True
 plot_seismicity  = True
@@ -67,7 +69,8 @@ fig.coast(
     projection=f"M20",
     region=region,
     shorelines='1p,black',
-    land = 'lightgray'
+    land = 'lightgray',
+    water='lightblue'
     
 )
 fig.plot(
@@ -97,8 +100,7 @@ if plot_seismicity:
     )
 
 spec="e60/0.39+f18"
-fig.plot(x=fault_cords.lon, y=fault_cords.lat, pen='1.8p,black', close=True)
-fig.plot(x=fault_cords.lon, y=fault_cords.lat, pen='1p,cyan', close=True)
+
 
 disp = gnss[['id', 'lon', 'lat', 'E', 'N']]
 
@@ -153,12 +155,74 @@ fig.text(x=-125.25, y=40.9, text='3 cm', font="10p", offset='0.8c/0.5c')
 fig.text(x=-125.25, y=40.87, text='Static inversion', font="10p", offset='3.5c/0.0c') 
 fig.text(x=-125.25, y=40.9, text='Data', font="10p", offset='3.5c/0.0c')
 
+fig.plot(x=span_2024["lon"], y=span_2024["lat"], pen = "10p,black@50%")
 
 
-
-fig.basemap(map_scale="n0.1/0.1+w10k+f+u")
+fig.basemap(map_scale="n0.07/0.81+w10k+f+u")
 fig.text(position='TL', no_clip=True, text='(c)', font='12p,Helvetica,black', offset='-0.8c/0.4c')
+#################################################################################################################################################################################################################################
+# inset
+region3=[-125.32, -124, 40.1, 40.7]
 
+fig.coast(
+    frame=['tblr', 'xa0.5f0.1', 'ya0.5f0.1'],
+    projection=f"M7",
+    region=region3,
+    land='lightgray',
+    water='lightblue'
+)
+fig.plot(
+    x=cat.lon,
+    y=cat.lat,
+    style="c0.05c",
+    fill="darkgray",
+    label='Lomax catalog'
+)
+
+fig.plot(data='../figure1/output.gmt', pen='1p,black', close=True)
+fig.plot(data='../figure1/SAF_Men.gmt', pen='2p,red')
+fig.plot(data='../figure1/cascedia.gmt', pen='2p,red')
+
+fig.text(x=-125.15, y=41.3, text='Cascadia Subduction Zone', font='12p,red', angle=-75)
+fig.text(x=-124.18, y=39.95, text='SAF', font='12p,red', angle=-75)
+fig.plot(x=-123.95, y=39.8, style="v0.5c+ea+r+h0.1+a35", direction=([105], [1.5]), pen="1p,red", fill="red")
+fig.plot(x=-124.15, y=40.05, style="v0.5c+ea+r+h0.1+a35", direction=([285], [1.5]), pen="1p,red", fill="red")
+fig.text(x=-125.9, y=40.33, text='Mendocino Fault Zone', font='12p,red', angle=0)
+
+focal_mechanism = [
+    [280, 84, -179, 189, 89, -6, 4.028, 19]  # Single event
+]
+
+# Create a DataFrame to pass to pygmt.meca
+focal_df = pd.DataFrame(focal_mechanism, columns=["strike1", "dip1", "rake1", "strike2", "dip2", "rake2", "mantissa", "exponent"])
+
+# Event information (longitude, latitude, depth) and location for beachball
+event_data = pd.DataFrame({
+    "longitude": [-125.022],
+    "latitude": [40.374],
+    "depth": [10.0]
+})
+
+# Add the focal mechanism DataFrame to the event DataFrame
+event_data = pd.concat([event_data, focal_df], axis=1)
+
+fig.plot(x=span_2024["lon"], y=span_2024["lat"], pen = "10p,black@50%")
+# Plot the beachball using GCMT convention
+fig.meca(spec=event_data, scale="1.5c", offset=True) 
+fig.text(x=-125.022, y=40.345, text='December 5, 2024 @[M_{\\textrm{w}}@[7.0', font='8p', angle=0, offset='0/-0.5c')
+
+fig.plot(x=-126.15, y=40.38, style="v0.5c+ea+r+h0.1+a35", direction=([0], [1.5]), pen="1p,red", fill="red")
+fig.plot(x=-125.83, y=40.45, style="v0.5c+ea+r+h0.1+a35", direction=([180], [1.5]), pen="1p,red", fill="red")
+fig.plot(x=back_projection.lon, y=back_projection.lat, style="dd", size=0.4 * back_projection.beam_power, fill=back_projection.time, cmap='../figure1/back_projection.cpt', pen='0.5p')
+
+
+# Add the color bar for depth
+with pygmt.config(FONT_LABEL='24p,Helvetica,black', FONT_ANNOT_PRIMARY='24p,Helvetica,black'):
+    fig.colorbar(
+        cmap='../figure1/back_projection.cpt',  # Use the same colormap as in the mec plot
+        position="JBR+w3c/0.3c+o0.1c/-3.5c",  # Position of the color bar
+        frame='af+ltime (s)' # Label the color bar
+    )
 
 ### panel b #####
 fig.shift_origin(yshift='16c')
@@ -217,7 +281,6 @@ fig.legend(position='n0.01/0.01', box='+ggray')
 
 
 
-fig.show(width=1200)
 
 
 fig.savefig('./figure2.pdf')
