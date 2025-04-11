0882a3b66ed2e949833fe14491f212c7501483d8
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2025-04-11 14:41:05 +0200

    make it work with manual processing

---
+++ seissol_setup/custom_setup_files/CFS_code_block.lua
@@ -0,0 +1,11 @@
+        fault_tag = x['fault_tag']
+        if fault_tag == 65 then
+           cohesion = -1e8
+           T_s = 1000.0
+           T_d = 0.0
+        end
+        if fault_tag ==66 then
+           cohesion = -1e8
+           T_s = 0.0
+           T_d = 1000.0
+        end
