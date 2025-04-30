3fc846ed9ed10da53e6355e70b1ab2967ab57c80
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2025-04-30 14:15:20 +0200

    figure comparing effect of velocity model

--- figures/figure_inversion_2_faults/generate_figure_comparison.sh
+++ figures/figure_inversion_2_faults/generate_figure_comparison.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-f1=/export/dump/ulrich/2024CapeMendocinoEarthquake/inversions/re98124.75_gps_tl_sm2
-f2=/export/dump/ulrich/2024CapeMendocinoEarthquake/inversions/rwca124.75_gps_tl_sm2/
+f1=/export/dump/ulrich/2024CapeMendocinoEarthquake/inversions/all_re98/re98124.75_gps_tl_sm2
+f2=/export/dump/ulrich/2024CapeMendocinoEarthquake/inversions/all_cascdia/rwca124.75_gps_tl_sm2/
+#change to the following for the figure checking the effect of the velocity model
+#f2=/export/dump/ulrich/2024CapeMendocinoEarthquake/inversions/all_usgs_vel/usgs_vel_gps_tl_sm2/
 ln -s $(realpath plot_graphic_NEIC_multiple.py) /export/dump/ulrich/neic-finitefault/src/wasp/plot_graphic_NEIC_multiple.py
 python /export/dump/ulrich/neic-finitefault/src/wasp/plot_graphic_NEIC_multiple.py "$f1,$f2" "body" --stations_body "LVZ,GRFO,PAB,MACI,SSPA,FDFM,MPG,SAML,PAYG"
 python /export/dump/ulrich/neic-finitefault/src/wasp/plot_graphic_NEIC_multiple.py "$f1,$f2" "strong" --stations_strong "KRP,THOM,KMPB,PETL,MNDO"
 python /export/dump/ulrich/neic-finitefault/src/wasp/plot_graphic_NEIC_multiple.py "$f1,$f2" "surf" --stations_surf "BORG,PAB,PAYG"
-wasp plot neic -t gps $2 --ffm-solution --map-limits " -125.5" " -123.0" 39.8 41.3 
-wasp plot neic -t gps $2 --tensor
+wasp plot neic -t gps $f2 --ffm-solution --map-limits " -125.5" " -123.0" 39.8 41.3
+wasp plot neic -t gps $f2 --tensor
 mv $f1/*.png .
 cp $f2/plots/SlipDist_plane0.png .
 cp $f2/plots/Map.png .
 cp $f2/plots/Cumulative_Moment_Tensor.png .
+cp $f2/plots/MomentRate.png .
