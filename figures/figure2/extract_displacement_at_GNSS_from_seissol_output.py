8943cc0e4e16026ee38cc21489b1573e1f858eca
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2025-04-30 11:25:51 +0200

    add gnss comparison

---
+++ figures/figure2/extract_displacement_at_GNSS_from_seissol_output.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import argparse
+import seissolxdmf
+from pyproj import Transformer
+import scipy.interpolate as interp
+
+
+def read_seissol_surface_data(xdmfFilename):
+    """read unstructured free surface output and associated data.
+    compute cell_barycenter"""
+    sx = seissolxdmf.seissolxdmf(xdmfFilename)
+    xyz = sx.ReadGeometry()
+    connect = sx.ReadConnect()
+
+    U = sx.ReadData("u1", sx.ndt - 1)
+    V = sx.ReadData("u2", sx.ndt - 1)
+    W = sx.ReadData("u3", sx.ndt - 1)
+
+    # project the data to geocentric (lat, lon)
+    myproj = "+proj=tmerc +datum=WGS84 +k=0.9996 +lon_0=-125.02 +lat_0=40.37"
+    transformer = Transformer.from_crs(myproj, "epsg:4326", always_xy=True)
+    lons, lats = transformer.transform(xyz[:, 0], xyz[:, 1])
+    xy = np.vstack((lons, lats)).T
+
+    # compute triangule barycenter
+    lonlat_barycenter = (
+        xy[connect[:, 0], :] + xy[connect[:, 1], :] + xy[connect[:, 2], :]
+    ) / 3.0
+
+    return lons, lats, lonlat_barycenter, connect, U, V, W
+
+
+def interpolate_seissol_surf_output(lonlat_barycenter, U, df):
+    """interpolate SeisSol free surface output to GPS data location"""
+
+    Fvsm = interp.LinearNDInterpolator(lonlat_barycenter, U)
+    locGPS = np.vstack((df["lon"].to_numpy(), df["lat"].to_numpy())).T
+
+    Fvsm = interp.LinearNDInterpolator(lonlat_barycenter, U)
+    ui = Fvsm.__call__(locGPS)
+    Fvsm = interp.LinearNDInterpolator(lonlat_barycenter, V)
+    vi = Fvsm.__call__(locGPS)
+    Fvsm = interp.LinearNDInterpolator(lonlat_barycenter, W)
+    wi = Fvsm.__call__(locGPS)
+    df["E_syn"] = ui
+    df["N_syn"] = vi
+    df["Up_syn"] = wi
+
+    return df
+
+parser = argparse.ArgumentParser(
+    description="extract displacement at GNSS station from seissol surface output"
+)
+parser.add_argument(
+    "--gnss_data", help="SeisSol xdmf surface file", default="gnss_data.csv"
+)
+
+parser.add_argument("seissol_surface_output", help="SeisSol xdmf surface file")
+
+
+args = parser.parse_args()
+
+# Read GPS data
+df = pd.read_csv(args.gnss_data, skiprows=[1])
+print(df)
+
+# Read SeisSol output and interpolate output to GPS data point locations
+lons, lats, lonlat_barycenter, connect, U, V, W = read_seissol_surface_data(
+    args.seissol_surface_output
+)
+df = interpolate_seissol_surf_output(lonlat_barycenter, U, df)
+print(df)
+# Select only the desired columns and rename them
+df_out = df[["id", "lon", "lat", "E_syn", "N_syn", "Up_syn"]].copy()
+df_out = df_out.rename(columns={"E_syn": "E", "N_syn": "N", "Up_syn": "Up"})
+df_out = df_out.dropna()
+
+df_out.to_csv("gnss_data_preferred_DR.csv", index=False)
+
+
---
+++ figures/figure2/plot_simple_gnss_comparison.py
@@ -0,0 +1,168 @@
+import pandas as pd
+import numpy as np
+import pygmt
+
+class Cpt:
+    def __init__(self, cpt):
+        cpt = pd.read_csv(cpt, sep='\t', header=None)
+        self.cpt_o = cpt.iloc[-3:]
+        cpt = cpt[:-3]
+        cpt[0] = np.array(cpt[0]).astype(float)
+        self.cpt = cpt
+    def __call__(self, value):
+        if value is None:
+            return self.cpt_o.iloc[2, 1]
+        idx = np.sum(self.cpt[0] < value) - 1
+        if idx < 0:
+            return self.cpt_o.iloc[0, 1]
+        elif idx + 2 > self.cpt.shape[0]:
+            return self.cpt_o.iloc[1, 1]
+        else:
+            return self.cpt.iloc[idx, 1]
+
+cat = pd.read_csv('./aftershocks.csv')
+cat['time'] = pd.to_datetime(cat['time'])
+cat = cat[cat.time > pd.to_datetime('2024-12-05T18:44:21')]
+
+       
+cat_on_fault = pd.read_csv('./aftershocks_on_fault.csv')
+cat_on_fault['time'] = pd.to_datetime(cat_on_fault['time'])
+cat_on_fault = cat_on_fault[cat_on_fault.time > pd.to_datetime('2024-12-05T18:44:21')]
+
+
+repeating_cat = pd.read_csv('./repeating_earthquakes.csv')
+background = pd.read_csv('./background_seismicity.csv')
+mainshock = pd.read_csv('./mainshock.csv')
+
+static_inv = pd.read_csv('./static_inv.csv')
+gnss = pd.read_csv('./gnss_data.csv')
+#static_inv_gnss_syn = pd.read_csv('./static_inv_gnss_syn.csv')
+static_inv_gnss_syn = pd.read_csv('gnss_data_preferred_DR.csv')
+fault_cords = pd.read_csv('./fault_cords.csv')
+
+kinematic_inv = pd.read_csv('./kinematic_inv.csv')
+kinematic_inv_gnss_syn = pd.read_csv('./kinematic_inv_gnss_syn.csv')
+
+
+cpt = Cpt('./slip.cpt')
+
+stf = pd.read_csv('./stf.csv')
+
+back_projection = pd.read_csv('../figure1/back_projection.csv')
+
+span_2024={'lon':[-125.247, -124.490], 'lat':[40.383, 40.303]}
+
+
+plot_kinematic = True
+plot_seismicity  = False
+
+
+region=[-125.3, -123.6, 40, 41]
+fig = pygmt.Figure()
+pygmt.config(FORMAT_GEO_MAP='ddd.xx', MAP_FRAME_TYPE='plain')
+
+#### panel c #####
+
+fig.coast(
+    frame=['lrtb'],
+    projection=f"M20",
+    region=region,
+    shorelines='1p,black',
+    land = 'lightgray',
+    water='lightblue'
+    
+)
+fig.plot(
+    data='./faults.gmt',
+    pen="1p,black",
+)
+
+if plot_seismicity:
+    cc = 100
+    fig.plot(
+        x=background.lon,
+        y=background.lat,
+        style="c0.05c",
+        fill=f'{cc}/{cc}/{cc}',
+    )
+    fig.plot(
+        x=repeating_cat.lon,
+        y=repeating_cat.lat,
+        style="c0.1c",
+        fill='red',
+    )
+    fig.plot(
+        x=cat.lon,
+        y=cat.lat,
+        style="c0.07c",
+        fill="cyan",
+    )
+
+spec="e60/0.39+f18"
+
+
+disp = gnss[['id', 'lon', 'lat', 'E', 'N']]
+
+fig.velo(
+    data=disp.iloc[:, 1:],
+    spec=spec,
+    pen="2p,red",
+    line=True,
+    vector="0.6c+e+gred",
+)
+fig.velo(
+    data=static_inv_gnss_syn[['lon', 'lat', 'E', 'N']],
+    spec=spec,
+    pen="2p,blue",
+    line=True,
+    vector="0.4c+e+gblue",
+)
+if plot_kinematic:
+    fig.velo(
+        data=kinematic_inv_gnss_syn[['lon', 'lat', 'E', 'N']],
+        spec=spec,
+        pen="2p,pink",
+        line=True,
+        vector="0.4c+e+gpink",
+    )
+size = 0.03
+fig.velo(
+    data=[[-125.25, 40.87, size, 0]],
+    spec=spec,
+    pen="2p,blue",
+    line=True,
+    vector="0.4c+e+gblue",
+)
+if plot_kinematic:
+    fig.velo(
+        data=[[-125.25, 40.84, size, 0]],
+        spec=spec,
+        pen="2p,pink",
+        line=True,
+        vector="0.4c+e+gpink",
+    )
+    fig.text(x=-125.25, y=40.84, text='Kinematic inversion', font="10p", offset='3.5c/0.0c') 
+
+fig.velo(
+    data=[[-125.25, 40.9, size, 0]],
+    spec=spec,
+    pen="2p,red",
+    line=True,
+    vector="0.6c+e+gred",
+)
+
+fig.text(x=-125.25, y=40.9, text='3 cm', font="10p", offset='0.8c/0.5c')
+fig.text(x=-125.25, y=40.87, text='Dynamic rupture model', font="10p", offset='3.8c/0.0c') 
+fig.text(x=-125.25, y=40.9, text='Data', font="10p", offset='2.35c/0.0c')
+
+fig.plot(x=span_2024["lon"], y=span_2024["lat"], pen = "10p,black@50%")
+
+
+fig.basemap(map_scale="n0.07/0.81+w10k+f+u")
+fig.text(position='TL', no_clip=True, text='(c)', font='12p,Helvetica,black', offset='-0.8c/0.4c')
+
+
+
+fn = './fig_gnss_comparison.pdf'
+fig.savefig(fn)
+print(f"done writing {fn}")
